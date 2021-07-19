c075e14f9217ef730029175896c5626f78e131a5
Yuri Monte <yuri.spm@gmail.com>
2021-07-19 20:32:48 -0300

    Conhecendo o blade - #04

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Yuri";
+    $idade = 29;
+    return view('welcome',["nome" => $nome, "idade" => $idade, "profissao" => "Programador"]);
 });
 
 Route::get('/contact', function () {
