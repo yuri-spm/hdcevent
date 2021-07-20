08406df987055f94c5e913bfa92f502009f41580
Yuri Monte <yuri.spm@gmail.com>
2021-07-20 10:55:23 -0300

    Utilizando o Eloquent do Laravel - #13

--- app/Http/Controllers/EventContoller.php
+++ app/Http/Controllers/EventContoller.php
@@ -4,22 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+
 class EventContoller extends Controller
 {
     public function index(){
-        $nome = "Yuri";
-        $idade = 29;
-    
-        $arr = [1,2,3,4,5];
-    
-        $nomes = ["Matheus", "Maria", "João", "Paulo"];
-    
-        return view('welcome',["nome" => $nome,
-         "idade" => $idade,
-         "profissao" => "Programador",
-        "arr" => $arr,
-        "nomes" => $nomes
-      ]);
+        $events = Event::all();
+
+        return view('welcome',['events' => $events]);
     }
 
     public function create(){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,37 +4,9 @@
 
 @section('content')
     
-
-    
-
-       <h1>Algum título</h1>
-       <img src="/img/banner.jpg" alt="Banner">
-       @if(10 < 5)
-          <p>A condição e true <p>
-       @endif
-      @if ($nome == "Pedro")
-          <p>O nome é Pedro</p>
-      @elseif($nome == "Yuri")
-          <p>O nome é {{$nome}} e ele tem {{$idade}} anos e trabalha como {{$profissao}}</p>
-      @else
-        <p>O nome não é Pedro</p>
-      @endif      
-      
-      @for ($i = 0; $i < count($arr); $i++)
-          <p>{{$arr[$i]}} - {{$i}}</p>
-          @if ($i == 2)
-             <p>O i e 2</p>
-          @endif
-      @endfor
-            
-      @php
-          $name = "João";
-          echo $name;
-      @endphp
-    @foreach ($nomes as $nome)
-        <p>{{$loop->index}}</p>
-        <p>{{$nome}}</p>
-    @endforeach
+@foreach ($events as $event)
+    <p>{{$event->title}} -- {{$event->descritpion}}</p>
+@endforeach
 
 
 @endsection
