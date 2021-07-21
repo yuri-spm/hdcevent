54d78dc3909648df9d18bf69059404082dd5dd65
Yuri Monte <yuri.spm@gmail.com>
2021-07-21 19:57:29 -0300

    Download do repositorio

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<h1>Esta e a pagina de contato</h1>
+
+<a href="/">Voltar para home </a>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\EventContoller;
+
+
+Route::get('/', [EventContoller::class, 'index']); 
+Route::get('/events/create', [EventContoller::class, 'create'])->middleware('auth');
+Route::get('/events/{id}', [EventContoller::class, 'show']);
+Route::post('/events',[EventContoller::class, 'store']);
+Route::get('/events/edit/{id}', [EventContoller::class, 'edit'])->middleware('auth');
+Route::put('/events/update/{id}',[EventContoller::class, 'update'])->middleware('auth');
+Route::delete('/events/{id}',[EventContoller::class, 'destroy'])->middleware('auth');;
+
+
+Route::get('/dashboard',[EventContoller::class, 'dashboard'])->middleware('auth');
