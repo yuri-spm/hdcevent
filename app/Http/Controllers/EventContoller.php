2575a2fe99175c09f71ad0a9efa4cec6035cf2b6
Yuri Monte <yuri.spm@gmail.com>
2021-07-23 08:54:45 -0300

    Exibindo eventos do usuário - #29

---
+++ app/Http/Controllers/EventContoller.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Event;
+
+use App\Models\User;
+
+class EventContoller extends Controller
+{
+    public function index(){
+
+        $search = request('search');
+        
+        if($search){
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']    
+            ])->get();
+        }else{
+            $events = Event::all();
+        }
+        return view('welcome',['events' => $events, 'search' => $search]);
+    }
+
+    public function create(){
+        return view('events.create');
+    }
+
+    public function store(Request $request){
+        $event = new Event;
+
+        $event->title = $request->title;
+        $event->date = $request->date;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+        $event->items = $request->items;
+
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+
+        }
+
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
+        $event->save();
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+
+    }
+
+    public function show($id){
+
+        $event = Event::findOrFail($id);
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray() ;
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+    }
+
+    public function dashboard(){
+        
+      
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        $eventsasparticipant = $user->eventsasparticipant;
+
+        return view('events.dashboard', ['events' => $events,
+                       'eventsasparticipant' => $eventsasparticipant]);
+    }
+
+    public function edit($id){
+
+        $user = auth()->user();
+
+        $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
+
+        return view('events.edit', ['event' => $event]);
+    }
+    
+    public function update(Request $request){
+
+        $data = $request->all();
+
+         // Image Upload
+         if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $data['image'] = $imageName;
+        
+        }
+
+        Event::findOrFail($request->id)->update($data);
+    
+        return redirect('/dashboard')->with('msg', 'Evento atualizado com sucesso.');
+
+    }
+
+    public function joinEvent($id){
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->attach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento' . $event->title);
+
+    }
+
+    public function destroy($id){
+
+        Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento ecluído com sucesso.');
+    }
+}
